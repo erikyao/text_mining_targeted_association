96a9b14bf659f565bc76917b7b2160568363d545
Edgar Gatica <EdgarGaticaCU@gmail.com>
2021-09-24 19:56:03 -0500

    Fixing the version.get_release, which was failing in prod.

--- version.py
+++ version.py
@@ -3,10 +3,4 @@ def get_release(self):
     # hard-coded release for demo purpose
     # "self" is a dumper instance, see:
     # https://github.com/biothings/biothings.api/blob/master/biothings/hub/dataload/dumper.py
-    import requests
-    res = requests.get("https://storage.googleapis.com/translator-text-workflow-dev-public/kgx/UniProt/nodes.tsv")
-    try:
-        last_modified = res.headers.get("Last-Modified", "1.0")
-        return last_modified
-    except:
-        return "1.0"
+    return "1.1"

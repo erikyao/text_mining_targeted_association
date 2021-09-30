9408c731907cd677735de2903defc84bcc9f4158
edgargaticaCU <88678635+edgargaticaCU@users.noreply.github.com>
2021-09-30 15:43:33 -0500

    Updating the version response. Changing the dumper config to use gzipped files.

--- version.py
+++ version.py
@@ -3,4 +3,12 @@ def get_release(self):
     # hard-coded release for demo purpose
     # "self" is a dumper instance, see:
     # https://github.com/biothings/biothings.api/blob/master/biothings/hub/dataload/dumper.py
-    return "1.1"
+    import requests
+    import datetime
+    res = requests.get("https://storage.googleapis.com/translator-text-workflow-dev-public/kgx/UniProt/nodes.tsv.gz")
+    try:
+        last_modified = res.headers.get("Last-Modified", "1.1")
+        dt = datetime.datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S %Z')
+        return dt.date().isoformat()
+    except:
+        return "1.1"
